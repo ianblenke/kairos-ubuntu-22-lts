de4993a733638fa151aa3c2d406a1ef85c8d4379
ianblenke <ian@blenke.com>
2023-11-12 15:01:27 -0500

    Force an image pull of the latest ubuntu:jammy before building

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 #export IMAGE=quay.io/kairos/kairos-ubuntu-22-lts:v2.4.1-k3sv1.27.3-k3s1
 export IMAGE=docker.io/ianblenke/kairos-ubuntu-22-lts:latest
 
+docker pull ubuntu:jammy
 DOCKER_BUILDKIT=1 docker build . --secret id=pro-attach-config,src=pro-attach-config.yaml -t ianblenke/kairos-ubuntu-22-lts -t $IMAGE
 docker push ianblenke/kairos-ubuntu-22-lts
 
